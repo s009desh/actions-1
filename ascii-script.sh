4dc8871415f1f15f5aa12b40ab3acce69b32307b
s009desh <63408926+s009desh@users.noreply.github.com>
2024-02-03 20:48:43 +0530

    corrected the shell script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo apt-get install cowsay.sh
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
